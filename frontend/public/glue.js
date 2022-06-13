13b8dab53cf57343bf4b9c6343198ee06c2f993a
michael-niewerth <michael.dominik.niewerth@gmail.com>
2022-06-14 00:06:01 +0200

    added back end directory

--- frontend/public/glue.js
+++ frontend/public/glue.js
@@ -1,8 +1,4 @@
 const invoke = window.__TAURI__.invoke
-export async function invokeHello(name){
-    return await invoke("hello",{name:name});
-}
-
-export async function invokeTest(name){
-    return await invoke("test",{name:name});
+export async function invokeBtn(id) {
+    return await invoke("btn",{id:id});
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,34 +2,82 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+use std::env;
+use std::{error::Error, time::SystemTime};
+use std::fs::OpenOptions;
+use csv::Writer;
+use std::path::Path;
+use std::fs::File;
 
 fn main() {
+  let active_id :u8 = 0;
   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![hello,test])
+      .invoke_handler(tauri::generate_handler![btn])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn buttonpress(id: u8) ->{
-  // This is a very simplistic example but it shows how to return a Result
-  // and use it in the front-end.
-  if name.contains(' ') {
-    Err("Name should not contain spaces".to_string())
-  } else {
-    Ok(format!("Hello, {}", name))
+fn btn(id: u8) -> Result<u8, u8> {
+  if active_id.equals(0:u8){
+    active_id = id;
+    wrt_id_start(id);
+  }else if active_id.equals(id) {
+    active_id=0;
+    wrt_end(id);
+  }else {
+    wrt_end(active_id);
+    wrt_id_start(id);
   }
+  Ok(active_id)
 }
 
-#[tauri::command]
-fn test(name: &str) -> Result<String, String> {
-  // This is a very simplistic example but it shows how to return a Result
-  // and use it in the front-end.
-  if name.contains(' ') {
-    Err("Name should not contain spaces".to_string())
-  } else {
-    Ok(format!("Hello, {}", name))
+
+fn wrt_id_start(id:u8){
+  if !Path::new("./data.csv").exists(){
+    createfile();
   }
+  let file = OpenOptions::new()
+      .write(true)
+      .append(true)
+      .open("data.csv")
+      .unwrap();
+  let mut writer = Writer::from_writer(file);
+  writer.write_field(id.to_string())?;
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+      Ok(n) => writer.write_field((n.as_secs().to_string()))?,
+      Err(_) => {
+        writer.write_field("time_error lmao")?;
+        panic!("SystemTime before UNIX EPOCH!");
+      },
+    }
+
+
 }
+
+fn wrt_end(id:u8){
+  if !Path::new("./data.csv").exists(){
+    createfile();
+  }
+  let file = OpenOptions::new()
+      .write(true)
+      .append(true)
+      .open("data.csv")
+      .unwrap();
+  let mut writer = Writer::from_writer(file);
+  writer.write_record(&[(match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+    Ok(n) => n.as_secs(),
+    Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+  }.to_string())])?;
+  writer.flush()?;
+  Ok(())
+}
+
+
+fn createfile() {
+  let mut file = File::create("data.csv")
+      .expect("Error encountered while creating file!");
+}
+
 
 
